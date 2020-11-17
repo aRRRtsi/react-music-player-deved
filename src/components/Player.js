2d6deb90609ab13ca48401661002c207da592794
Ari Leskinen <arrrtsi@gmail.com>
2020-11-17 20:29:14 +0200

    FontAwesome
    Installed font awesome with instructions from https://fontawesome.com/how-to-use/on-the-web/using-with/react. Tested icons in Playerjs.

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,3 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPlayCircle,
+  faChevronCircleRight,
+  faChevronCircleLeft,
+} from '@fortawesome/free-solid-svg-icons';
+
 const Player = () => {
   return (
     <div className='player-container'>
@@ -6,7 +13,23 @@ const Player = () => {
         <input type='range' name='' id='' />
         <p>End time</p>
       </div>
-      <div className='play-control'></div>
+      <div className='play-control'>
+        <FontAwesomeIcon
+          className='play-control-previous'
+          icon={faChevronCircleLeft}
+          size='2x'
+        />
+        <FontAwesomeIcon
+          className='play-control-play'
+          icon={faPlayCircle}
+          size='2x'
+        />
+        <FontAwesomeIcon
+          className='play-control-next'
+          icon={faChevronCircleRight}
+          size='2x'
+        />
+      </div>
     </div>
   );
 };
